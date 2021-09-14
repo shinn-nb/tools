260e402fa4c24b6a098e02fdf6acbfcf841f65f5
nannanshi <nannanshi@yufuid.com>
2021-09-14 15:45:08 +0800

    v2.1 自动生成maven结果

--- pom-config/src/main/java/com/snn/tools/pomconfig/panel/MainPanel.java
+++ pom-config/src/main/java/com/snn/tools/pomconfig/panel/MainPanel.java
@@ -3,8 +3,10 @@ package com.snn.tools.pomconfig.panel;
 import com.snn.tools.pomconfig.entity.JarConfig;
 import com.snn.tools.pomconfig.utils.TxtUtil;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.shared.invoker.*;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.io.File;
@@ -19,6 +21,7 @@ import java.util.stream.Collectors;
 public class MainPanel {
     private MainPanel() {}
 
+    private static final String BUILD_TREE = "dependency:tree";
     private static final Object[]
         COLUMN_NAMES =
         new Object[]{"序号",
@@ -32,41 +35,46 @@ public class MainPanel {
 
     public static void initModel() {
         JFrame frame = new JFrame("版本比较");
-        frame.setSize(400, 300);
+        frame.setSize(450, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
         frame.add(panel);
         panel.setLayout(null);
-        JLabel fileLabel1 = new JLabel("版本来源路径：");
-        JLabel fileLabel2 = new JLabel("版本比较路径：");
+        JLabel fileLabel1 = new JLabel("版本来源pom路径：");
+        JLabel fileLabel2 = new JLabel("版本比较pom路径：");
         JTextField fileText1 = new JTextField(20);
-        fileText1.setText("/Users/anan/Documents/workspace/idaas-core/tree.txt");
+        fileText1.setText("/Users/anan/Documents/workspace/idaas-core/pom.xml");
         JTextField fileText2 = new JTextField(20);
-        fileText2.setText("/Users/anan/Documents/workspace/idaas-lcm/tree.txt");
+        fileText2.setText("/Users/anan/Documents/workspace/idaas-lcm/pom.xml");
 
-        fileLabel1.setBounds(10, 20, 100, 35);
+        fileLabel1.setBounds(10, 20, 150, 35);
         panel.add(fileLabel1);
-        fileText1.setBounds(120, 20, 250, 35);
+        fileText1.setBounds(165, 20, 250, 35);
         panel.add(fileText1);
 
-        fileLabel2.setBounds(10, 60, 100, 35);
+        fileLabel2.setBounds(10, 60, 150, 35);
         panel.add(fileLabel2);
-        fileText2.setBounds(120, 60, 250, 35);
+        fileText2.setBounds(165, 60, 250, 35);
         panel.add(fileText2);
         Font f = new Font("宋体", Font.PLAIN, 16);
 
-        JLabel text = new JLabel("路径是workSpace通过 maven 命令生成的txt的地址");
+        JLabel text = new JLabel("maven路径:");
         panel.add(text);
-        text.setBounds(10, 110, 400, 35);
-        JLabel mvn = new JLabel("命令：mvn dependency:tree --> tree.txt ");
+        text.setBounds(10, 110, 150, 35);
+        JTextField mvn = new JTextField(20);
         panel.add(mvn);
-        mvn.setBounds(10, 150, 400, 35);
+        mvn.setBounds(165, 110, 250, 35);
 
-        text.setFont(f);
-        text.setForeground(Color.red);
-        mvn.setFont(f);
-        mvn.setForeground(Color.red);
+        fileLabel1.setFont(f);
+        fileLabel1.setForeground(Color.red);
+        fileText1.setFont(f);
+        fileText1.setForeground(Color.red);
+
+        fileLabel2.setFont(f);
+        fileLabel2.setForeground(Color.red);
+        fileText2.setFont(f);
+        fileText2.setForeground(Color.red);
 
         JCheckBox jCheckBox = new JCheckBox("是否含第三方引用");
         jCheckBox.setBounds(10, 190, 160, 35);
@@ -76,36 +84,95 @@ public class MainPanel {
         panel.add(loginButton);
         panel.add(jCheckBox);
         loginButton.addActionListener(e -> {
-            String file1 = fileText1.getText();
-            String file2 = fileText2.getText();
-            if (StringUtils.isAnyBlank(file1, file2)) {
-                JOptionPane.showMessageDialog(frame, "存在路径未填写",
+            try {
+                loginButton.setText("正在比较...");
+                loginButton.setEnabled(false);
+                String file1 = fileText1.getText();
+                String file2 = fileText2.getText();
+                if (StringUtils.isAnyBlank(file1, file2)) {
+                    JOptionPane.showMessageDialog(frame, "存在路径未填写",
+                        "警告", JOptionPane.ERROR_MESSAGE
+                    );
+                    return;
+                }
+                if (!StringUtils.endsWith(file1, "pom.xml")) {
+                    file1 = file1 + "/pom.xml";
+                }
+                if (!StringUtils.endsWith(file2, "pom.xml")) {
+                    file2 = file2 + "/pom.xml";
+                }
+                if (!new File(file1).exists() || !new File(file2).exists()) {
+                    JOptionPane.showMessageDialog(frame, "请检查路径文件是否存在",
 
-                    "警告", JOptionPane.ERROR_MESSAGE
+                        "警告", JOptionPane.ERROR_MESSAGE
+                    );
+                    return;
+                }
+                List<Object[]> data = compareData(file1, file2, mvn.getText(), jCheckBox.isSelected());
+                Object[][] tableData = new Object[data.size()][];
+                for (int i = 0, len = data.size(); i < len; i++) {
+                    tableData[i] = data.get(i);
+                }
+                DefaultTableModel tableModel2 = new DefaultTableModel(tableData, COLUMN_NAMES);
+                TableFrame.showTable(tableModel2);
+            } catch (Exception e2) {
+                JOptionPane.showMessageDialog(frame, "生成失败,请查看输出的报错信息",
+                    "错误", JOptionPane.ERROR_MESSAGE
                 );
-                return;
+            } finally {
+                loginButton.setText("比较");
+                loginButton.setEnabled(true);
             }
-            if(!new File(file1).exists()||!new File(file2).exists()){
-                JOptionPane.showMessageDialog(frame, "请检查路径文件是否存在",
 
-                    "警告", JOptionPane.ERROR_MESSAGE
-                );
-                return;
-            }
-            List<Object[]> data = compareData(file1, file2, jCheckBox.isSelected());
-            Object[][] tableData = new Object[data.size()][];
-            for (int i = 0, len = data.size(); i < len; i++) {
-                tableData[i] = data.get(i);
-            }
-            DefaultTableModel tableModel2 = new DefaultTableModel(tableData, COLUMN_NAMES);
-            TableFrame.showTable(tableModel2);
         });
         frame.setVisible(true);
     }
 
-    private static List<Object[]> compareData(String file1, String file2, boolean hasThird) {
-        JarConfig jarConfig = TxtUtil.readTxt(file1);
-        JarConfig jarConfig11 = TxtUtil.readTxt(file2);
+    /**
+     * @param mavenPath  maven系统路径 如D:\apache-maven-3.5.4
+     * @param pomPath    要操控的pom文件的系统路径 如：D:\coding\**\pom.xml
+     * @param mavenOrder maven命令如：clean
+     */
+    public static JarConfig operationMavenOrder(String mavenPath, String pomPath, String mavenOrder) {
+
+        InvocationRequest request = new DefaultInvocationRequest();
+        //想要操控的pom文件的位置
+        request.setPomFile(new File(pomPath));
+        //操控的maven命令
+        request.setGoals(Collections.singletonList(mavenOrder));
+
+        Invoker invoker = new DefaultInvoker();
+        //maven的位置
+        if (!StringUtils.isBlank(mavenPath)) {
+            invoker.setMavenHome(new File(mavenPath));
+        }
+
+        TxtUtil util = new TxtUtil();
+        invoker.setOutputHandler(line -> util.readTxt(line));
+        try {
+            int a = invoker.execute(request).getExitCode();
+            if (a != 0) {
+                throw new RuntimeException("maven命令执行失败");
+            }
+            return util.getJarConfig();
+        } catch (
+            MavenInvocationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private static List<Object[]> compareData(String pomFile1, String pomFile2, String mavenPath, boolean hasThird) {
+
+        JarConfig jarConfig = operationMavenOrder(
+            mavenPath,
+            pomFile1,
+            BUILD_TREE
+        );
+        JarConfig jarConfig11 = operationMavenOrder(
+            mavenPath,
+            pomFile2,
+            BUILD_TREE
+        );
 
         Map<String, JarConfig> map1 = new HashMap<>();
         Map<String, JarConfig> map2 = new HashMap<>();
--- pom-config/src/main/java/com/snn/tools/pomconfig/utils/TxtUtil.java
+++ pom-config/src/main/java/com/snn/tools/pomconfig/utils/TxtUtil.java
@@ -13,65 +13,62 @@ import java.util.ArrayList;
  * @Date: 2021/9/13 上午11:52 （日期和时间）
  */
 public class TxtUtil {
-    private TxtUtil() {}
 
-    private static final String MODULE_SPLIT = "@";
-    private static final String JAR_START = "+-";
-    private static final String JAR_END = "\\-";
-    private static final String JAR_CONFIG_SPLIT = ":";
+    private final String MODULE_SPLIT = "@";
+    private final String JAR_START = "+-";
+    private final String JAR_END = "\\-";
+    private final String JAR_CONFIG_SPLIT = ":";
+    private JarConfig jarFirst;
+    private String module = null;
 
-    public static JarConfig readTxt(String filePath) {
-        JarConfig jarFirst = JarConfig.builder().number(-2).next(new ArrayList<>()).build();
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String tempStr;
-            String module = null;
+    JarConfig jarConfig = null;
+    boolean nextModule = false;
+    int lastReadNum = -1;
 
-            JarConfig jarConfig = null;
-            boolean nextModule = false;
-            int lastReadNum = -1;
-            while ((tempStr = reader.readLine()) != null) {
-                if (StringUtils.contains(tempStr, MODULE_SPLIT) && StringUtils.contains(
-                    tempStr,
-                    "maven-dependency-plugin:"
-                )) {
-                    module = StringUtils.remove(StringUtils.split(tempStr, MODULE_SPLIT)[1]," ---");
-                    jarConfig = JarConfig.builder().module(module).next(new ArrayList<>()).build();
-                    nextModule = true;
-                    continue;
-                }
-                if (nextModule && StringUtils.containsAny(tempStr, ":jar:", ":pom:")) {
-                    String[] modules = StringUtils.split(tempStr, JAR_CONFIG_SPLIT);
-                    jarConfig.setPre(jarFirst);
-                    jarConfig.setGroupId(modules[0]);
-                    jarConfig.setModule(module);
-                    jarConfig.setArtifactId(modules[1]);
-                    jarConfig.setVersion(modules[3]);
-                    jarFirst.getNext().add(jarConfig);
-                    jarConfig.setNumber(-1);
-                    lastReadNum=-1;
-                    nextModule = false;
-                    continue;
-                }
-                if (StringUtils.containsAny(tempStr, JAR_START, JAR_END)) {
-                    assert jarConfig != null;
-                    int num = getNum(tempStr);
-                    if (num == lastReadNum) {
-                        jarConfig = jarConfig.getPre();
-                    } else if (num < lastReadNum) {
-                        jarConfig = getPreJarByNum(jarConfig, num - 1);
-                    }
-                    jarConfig = dealNext(tempStr, jarConfig);
-                    lastReadNum = jarConfig.getNumber();
-                }
+    public TxtUtil() {
+        jarFirst = JarConfig.builder().number(-2).next(new ArrayList<>()).build();
+    }
+
+    public JarConfig readTxt(String tempStr) {
+        if (StringUtils.contains(tempStr, MODULE_SPLIT) && StringUtils.contains(
+            tempStr,
+            "maven-dependency-plugin:"
+        )) {
+            module = StringUtils.remove(StringUtils.split(tempStr, MODULE_SPLIT)[1], " ---");
+            jarConfig = JarConfig.builder().module(module).next(new ArrayList<>()).build();
+            nextModule = true;
+        }
+        if (nextModule && StringUtils.containsAny(tempStr, ":jar:", ":pom:")) {
+            String[] modules = StringUtils.split(tempStr, JAR_CONFIG_SPLIT);
+            jarConfig.setPre(jarFirst);
+            jarConfig.setGroupId(modules[0]);
+            jarConfig.setModule(module);
+            jarConfig.setArtifactId(modules[1]);
+            jarConfig.setVersion(modules[3]);
+            jarFirst.getNext().add(jarConfig);
+            jarConfig.setNumber(-1);
+            lastReadNum = -1;
+            nextModule = false;
+        }
+        if (StringUtils.containsAny(tempStr, JAR_START, JAR_END)) {
+            assert jarConfig != null;
+            int num = getNum(tempStr);
+            if (num == lastReadNum) {
+                jarConfig = jarConfig.getPre();
+            } else if (num < lastReadNum) {
+                jarConfig = getPreJarByNum(jarConfig, num - 1);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage());
+            jarConfig = dealNext(tempStr, jarConfig);
+            lastReadNum = jarConfig.getNumber();
         }
         return jarFirst;
     }
 
-    private static JarConfig getPreJarByNum(JarConfig jarConfig, int num) {
+    public JarConfig getJarConfig() {
+        return jarFirst;
+    }
+
+    private JarConfig getPreJarByNum(JarConfig jarConfig, int num) {
         jarConfig = jarConfig.getPre();
         while (jarConfig.getNumber() != num && jarConfig.getPre() != null) {
             jarConfig = jarConfig.getPre();
@@ -79,18 +76,18 @@ public class TxtUtil {
         return jarConfig;
     }
 
-    private static void dealJarDetail(String tempStr, JarConfig jarConfig) {
+    private void dealJarDetail(String tempStr, JarConfig jarConfig) {
         String[] modules = StringUtils.split(tempStr, JAR_CONFIG_SPLIT);
         jarConfig.setGroupId(modules[0]);
         jarConfig.setArtifactId(modules[1]);
         jarConfig.setVersion(modules[3]);
     }
 
-    private static int getNum(String tempStr) {
+    private int getNum(String tempStr) {
         return tempStr.length() - StringUtils.replace(tempStr, "|", "").length();
     }
 
-    private static JarConfig dealNext(String tempStr, JarConfig preJarConfig) {
+    private JarConfig dealNext(String tempStr, JarConfig preJarConfig) {
         String repStr = StringUtils.replace(tempStr, "|", "");
         int num = tempStr.length() - repStr.length();
         repStr = StringUtils.remove(repStr, " ");
