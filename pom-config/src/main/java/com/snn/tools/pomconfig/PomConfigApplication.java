fef5dea45ccf591145db8da43ff5f61dc2e3e92a
nannanshi <nannanshi@yufuid.com>
2021-09-14 10:31:48 +0800

    v1.0

---
+++ pom-config/src/main/java/com/snn/tools/pomconfig/PomConfigApplication.java
@@ -0,0 +1,15 @@
+package com.snn.tools.pomconfig;
+
+import com.snn.tools.pomconfig.panel.MainPanel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomConfigApplication {
+
+    public static void main(String[] args) {
+        MainPanel.initModel();
+        SpringApplication.run(PomConfigApplication.class, args);
+    }
+
+}
